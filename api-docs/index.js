237fddc2fa2470874cbda6abbd9a69aaaa9eafbf
Milos Mosovsky <milos.mosovsky@toptal.com>
2016-11-15 00:59:00 +0100

    fixup! Initial Commit

--- api-docs/index.js
+++
@@ -1,9 +0,0 @@
-import VDom from './lib/VDom';
-import Plasmatic from './lib/Plasmatic';
-import Component from './lib/interfaces/Component';
-
-export default {
-  Component,
-  App: Plasmatic,
-  createComponent: VDom.createComponent
-};
--- api-docs/lib/Types.js
+++
@@ -1,1 +0,0 @@
-import PlasmaticNode from './PlasmaticNode';
